4786dfaaa9ffd1fa9709f92e41df09ebcf51dad8
ozlucinar <cinarozlu@gmail.com>
2022-10-12 00:23:28 +0300

    refactor: change gui

--- src/App.py
+++ src/App.py
@@ -1,25 +1,15 @@
-from tkinter import Tk, Toplevel, Label, Button
+from tkinter import Tk, Label, Button
 from tkinter import filedialog
 from utils.predict import predict
-
-
 win = Tk()
+win.geometry("500x300")
 
-win.geometry("800x450")
-
-
-def open_file():
-    filepath = filedialog.askopenfilename(title="Open an Audio File", filetypes=(
-        ("audio", "*.mp3"), ("audio", "*.mp3")))
-    result = predict(filepath)
-    top = Toplevel(win)
-    top.geometry("750x250")
-    top.title("Result Window")
-    Label(top, text=result, font=('Mistral 18 bold')).place(x=375, y=125)
-
-
-button = Button(win, text="Predict", command=open_file)
-button.place(x=400, y=225)
-button.pack()
-
+label = Label(win, text="Open a File", font=('Mistral 18 bold'))
+label.grid()
+label.pack()
+filepath = filedialog.askopenfilename(title="Open an Audio File", filetypes=(
+    ("audio", "*.mp3"), ("audio", "*.mp3")))
+result = predict(filepath)
+print(result)
+label.config(text=result)
 win.mainloop()
